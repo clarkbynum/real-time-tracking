bc7502b99424281ab1d7202ebf6c278769c61b0c
Clark Bynum <sh3nan1gans@users.noreply.github.com>
2019-02-19 13:21:44 -0600

    Asset created by sh3nan1gans

---
+++ code/services/fetchViews/fetchViews.js
@@ -0,0 +1,77 @@
+/**
+ * @typedef {object} views
+ * @property {string} name
+ * @property {string} icon
+ * @property {number} width
+ * @property {number} height
+ */
+
+ /**
+ * @typedef {object} fetchViewsParams
+ * @property {string} cbQuery
+ * @property {number} pageSize
+ * @property {number} pageNum
+ */
+
+ /**
+ * Fetches a list of items from the "views" collection
+ * @param {{ params: fetchViewsParams | views }} req
+ * @param {fetchViewsParams | views} req.params
+ * @param {CbServer.Resp} resp
+ */
+function fetchViews(req, resp) {
+  log(req)  
+  ClearBlade.init({ request: req });
+
+  var cbQuery = req.params.cbQuery
+  var pageSize = req.params.pageSize || 0
+  var pageNum = req.params.pageNum || 0
+  delete req.params.cbQuery
+  delete req.params.pageSize
+  delete req.params.pageNum
+
+  var query = ClearBlade.Query();
+  // allows for complex queries generated from the filter widget
+  if (cbQuery) {
+    query.query = cbQuery
+  } else {
+    // allows basic filtering on any column
+    ["name", "icon", "width", "height"].forEach(function(name) {
+      value = req.params[name];
+      if(value !== undefined) {
+        query.equalTo(name, value);
+      }
+    })
+    query.setPage(pageSize, pageNum);
+  }
+
+  var fetchedData;
+  var count
+
+  var col = ClearBlade.Collection({ collectionName: "views" });
+  col.fetch(query, function (err, data) {
+    fetchedData = data
+    log(data)
+    if (err) {
+      resp.error(data)
+    } else {
+      // TOTAL from fetch is just DATA.length, replace it with total that match query
+      if(fetchedData && count !== undefined) {
+        fetchedData.TOTAL = count
+        resp.success(data)
+      }
+    }
+  });
+
+  col.count(query, function (err, data) {
+    count = data.count
+    if (err) {
+      resp.error(data)
+    } else {
+      if(fetchedData && count !== undefined) {
+        fetchedData.TOTAL = count
+        resp.success(fetchedData)
+      }
+    }
+  })
+}
